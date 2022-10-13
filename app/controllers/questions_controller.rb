0edb45b1949832772506fb87fe4a3244f18c0822
Victoria Torelli <v.torelli@icloud.com>
2022-10-13 18:10:00 +0200

    added css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,9 +8,9 @@ class QuestionsController < ApplicationController
     if @question.blank?
       @answer = "I can't hear you!"
     elsif @question.ends_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work! Chop Chop!"
     elsif @question =~ /i am going to work/i
-      @answer = "Great!"
+      @answer = "Finally, I thought this day would never come..."
     else
       @answer = "I don't care, get dressed and go to work!"
     end
